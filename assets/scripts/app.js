abed16b9bba94c8126e5b48852c4bb60f2a20180
NhungTranPY <nhungtranpy91@gmail.com>
2022-10-23 20:24:05 +0700

    20-25: bacsics JS

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,14 @@
-alert('this works')
+const defaultResult = 0
+
+let currentResult = defaultResult
+
+currentResult = (currentResult + 10) * 3 / 2 - 1
+
+let calculationDescription = '(currentResult + 10) * 3 / 2 - 1'
+
+let calculationDescription2 = '(' + defaultResult + ' 10) * 3 / 2 - 1'
+
+let calculationDescription3 = `( ${defaultResult} + 10) * 3 / 2 - 1` // same result as calculationDescription2
+let errorMessage = 'An error occured'
+
+outputResult(currentResult, calculationDescription3)
