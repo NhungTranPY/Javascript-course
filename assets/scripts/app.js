eda8f8c73272185f6c58708e4a5a9f773785ab25
NhungTranPY <nhungtranpy91@gmail.com>
2022-10-23 22:21:26 +0700

    36. Converting datatypes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,22 +1,26 @@
 const defaultResult = 0;
-let currentResult;
+let currentResult = defaultResult;
 
-function add(num1, num2) {
-    const result = num1 + num2
+function add() {
+    currentResult = currentResult + parseInt(userInput.value)
+    outputResult(currentResult, '')
     // alert(`The result is ${result}`)
     // alert('The result is' + result) // same as above, different syntax (written)
-    return result
+    // return result
     // the return statement ends the funtion execution, which means code thereafter isn't executed
     // alert('Hi there') // this line of code won't be executed
     // return // if return alone like this that means returns nothing, just to cancel function execution
-    
+
 }
 
 // const additionResult = add(1, 4)
 // currentResult = additionResult 
 // 2 dong tren co the viet nhu sau:
-currentResult = add(1, 4)
+// currentResult = add(1, 4)
 
-outputResult(currentResult, '')
+
+addBtn.addEventListener('click', add) // the second argument is a func that tells JS what to execute when 'click' occured
+
+
 
 
