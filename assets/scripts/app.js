687d82f7f97e0f8b13b7c382ffe3b7454a19c44f
NhungTranPY <nhungtranpy91@gmail.com>
2022-10-23 20:45:15 +0700

    27. Adding A Custom Function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,14 @@
 const defaultResult = 0
-
 let currentResult = defaultResult
+
+function add(num1, num2) {
+    const result = num1 + num2
+    alert(`The result is ${result}`)
+    // alert('The result is' + result) // same as above, different syntax (written)
+}
+
+add(1, 4)
+add(4, 7)
 
 currentResult = (currentResult + 10) * 3 / 2 - 1
 
@@ -12,3 +20,4 @@ let calculationDescription3 = `( ${defaultResult} + 10) * 3 / 2 - 1` // same res
 let errorMessage = 'An error occured'
 
 outputResult(currentResult, calculationDescription3)
+
