6bc2e7efc28d55afedb3651ff50b3072b660f9fb
NhungTranPY <nhungtranpy91@gmail.com>
2022-10-25 20:20:44 +0700

    39. Connecting all Buttons to Functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,16 +5,45 @@ function getUserNumberInput() {
     return parseInt(userInput.value)
 }
 
-function add() {
-    const enteredNumber = getUserNumberInput()
-    const calDescription = `${currentResult} + ${enteredNumber}`
-    currentResult = currentResult + enteredNumber
-    outputResult(currentResult, calDescription)
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`
+    outputResult(currentResult, calcDescription)
 }
 
+function add() {
+    const enteredNumber = getUserNumberInput()
+    const initialResult = currentResult
+    currentResult = currentResult + enteredNumber
+    createAndWriteOutput('+', initialResult, enteredNumber)
+}
+
+function subtract() {
+    const enteredNumber = getUserNumberInput()
+    // const calDescription = `${currentResult} - ${enteredNumber}`
+    const initialResult = currentResult
+    currentResult = currentResult - enteredNumber
+    // outputResult(currentResult, calDescription)
+    createAndWriteOutput('-', initialResult, enteredNumber)
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput()
+    const initialResult = currentResult
+    currentResult = currentResult * enteredNumber
+    createAndWriteOutput('*', initialResult, enteredNumber)
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput()
+    const initialResult = currentResult
+    currentResult = currentResult / enteredNumber
+    createAndWriteOutput('/', initialResult, enteredNumber)
+}
 
 addBtn.addEventListener('click', add) // the second argument is a func that tells JS what to execute when 'click' occured
-
+subtractBtn.addEventListener('click', subtract)
+multiplyBtn.addEventListener('click', multiply)
+divideBtn.addEventListener('click', divide)
 
 
 
