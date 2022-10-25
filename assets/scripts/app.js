0bea80555c0616826e4883648794c91aefc521f6
NhungTranPY <nhungtranpy91@gmail.com>
2022-10-25 21:28:19 +0700

    43. Using array

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,27 +1,32 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = []
 
+// gets input from input field
 function getUserNumberInput() {
     return parseInt(userInput.value)
 }
 
+// generate & write calculation log
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
     const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`
-    outputResult(currentResult, calcDescription)
+    outputResult(currentResult, calcDescription) // from vendor file
 }
 
 function add() {
     const enteredNumber = getUserNumberInput()
     const initialResult = currentResult
-    currentResult = currentResult + enteredNumber
+    currentResult += enteredNumber // same as currentResult = currentResult + enteredNumber
     createAndWriteOutput('+', initialResult, enteredNumber)
+    logEntries.push(enteredNumber)
+    console.log(logEntries[0]);
 }
 
 function subtract() {
     const enteredNumber = getUserNumberInput()
     // const calDescription = `${currentResult} - ${enteredNumber}`
     const initialResult = currentResult
-    currentResult = currentResult - enteredNumber
+    currentResult -= enteredNumber // same as currentResult = currentResult - enteredNumber
     // outputResult(currentResult, calDescription)
     createAndWriteOutput('-', initialResult, enteredNumber)
 }
@@ -29,14 +34,14 @@ function subtract() {
 function multiply() {
     const enteredNumber = getUserNumberInput()
     const initialResult = currentResult
-    currentResult = currentResult * enteredNumber
+    currentResult *= enteredNumber // same as currentResult = currentResult * enteredNumber
     createAndWriteOutput('*', initialResult, enteredNumber)
 }
 
 function divide() {
     const enteredNumber = getUserNumberInput()
     const initialResult = currentResult
-    currentResult = currentResult / enteredNumber
+    currentResult /= currentResult / enteredNumber // same as currentResult = currentResult / enteredNumber
     createAndWriteOutput('/', initialResult, enteredNumber)
 }
 
